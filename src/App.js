1c8b215c8227611318a37841ae1fa61177a4b7a0
harun <harun.khairy@gmail.com>
2020-04-02 10:37:07 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
+import Login from "./components/Login"
+import Register from './components/Register';
+import Footer from './components/Footer';
+
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div><Header /></div>
+      <div className="d-flex justify-content-center align-items-center" ><Login /></div>
+      <div className="d-flex justify-content-center align-items-center" ><Register /></div>
+      <div className="d-flex justify-content-center align-items-center" ><Footer /></div>
+      
+      
+      
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+class Home extends Component {
+
+    state = {
+        products: []
+    }
+
+
+
+    render () {
+        return (
+            <div>Ini Home</div>
+        )
+    }
+}
